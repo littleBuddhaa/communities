ece43c4ccba6f301b0a43b62e6219afd7f1306ab
Anumeha Agrawal <anuzenith29@gmail.com>
2018-08-12 10:29:21 +0530

    Revert "Travis ng test support (#90)"
    This reverts commit 25989d95e0d227bc74c35da76b95f7cbe07fe43c.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA } from '@angular/core';
 import { HomeComponent } from './home.component';
 
-xdescribe('HomeComponent', () => {
+describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
