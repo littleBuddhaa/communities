e6d98bfcb55ce6ba057b3c00974dbcbcf9d06ab0
tomtomato <tomzacchia@live.com>
2018-09-04 16:20:22 -0400

    ressolved issue 106 and fixed alignment of footer text

--- dist/communities/polyfills.js
+++ dist/communities/polyfills.js
@@ -5467,7 +5467,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/anumeha/Documents/communities/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/tomtom/openSourceProjects/communities/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
